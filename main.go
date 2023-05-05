2ec03750c1f325e8e983051e62cab21d668831d6
unknown <mhmmdazmye@gmail.com>
2023-05-05 16:17:42 +0700

    done handling transaction

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"waysfood/database"
 	"waysfood/pkg/mysql"
 	"waysfood/routes"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
@@ -18,12 +20,20 @@ func main() {
 
 	e := echo.New()
 
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.POST, echo.PATCH, echo.DELETE},
+		AllowHeaders: []string{"X-Requested-With", "Content-Type", "Authorization"},
+	}))
+
 	mysql.DatabaseInit()
 	database.RunMigration()
 
 	routes.RouteInit(e.Group("/api/v1"))
 
-	fmt.Println("Server Running on localhost:5000")
-	e.Logger.Fatal(e.Start("localhost:5000"))
+	var PORT = os.Getenv("PORT")
+
+	fmt.Println("Server is runnning on localhost:" + PORT)
+	e.Logger.Fatal(e.Start(":" + PORT))
 
 }
