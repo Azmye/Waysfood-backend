c70c62804413fa3138b8f7631459e5fb6ab566cd
unknown <mhmmdazmye@gmail.com>
2023-05-13 14:24:58 +0700

    first commit

--- dto/cart/cart_request.go
+++ dto/cart/cart_request.go
@@ -1,10 +1,10 @@
 package cartDto
 
 type CreateCartRequest struct {
-	ProductID  uint   `json:"product_id" form:"product_id" gorm:"not null"`
-	CustomerID uint   `json:"customer_id" form:"customer_id" gorm:"not null"`
-	Quantity   uint   `json:"qty" form:"qty" gorm:"not null"`
-	Status     string `json:"status" form:"qty" gorm:"not null"`
+	ProductID  uint   `json:"product_id" form:"product_id"`
+	CustomerID uint   `json:"customer_id" form:"customer_id"`
+	Quantity   uint   `json:"qty" form:"qty"`
+	Status     string `json:"status" form:"qty"`
 }
 
 type UpdateCartRequest struct {
