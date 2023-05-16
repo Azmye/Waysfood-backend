ad4cef1e466982b59296c29e167d5d37e2ee4290
unknown <mhmmdazmye@gmail.com>
2023-05-16 11:17:41 +0700

    handling transaction routes

--- models/transaction.go
+++ models/transaction.go
@@ -7,11 +7,9 @@ import (
 type Transaction struct {
 	gorm.Model
 	TransactionID int    `gorm:"not null"`
-	ProductID     uint   `gorm:"not null"`
 	CustomerID    uint   `gorm:"not null"`
 	Status        string `gorm:"not null"`
 	Location      string `gorm:"not null"`
 	TotalPrice    int    `gorm:"not null"`
-	Product       Product
 	Customer      User
 }
